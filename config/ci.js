e197ffcb9668b48c84f27b8ff9e7a9f529614e75
Your Name <you@example.com>
2023-12-24 03:56:12 -0800

    removed port from mongo URI

--- config/ci.js
+++ config/ci.js
@@ -4,7 +4,7 @@ module.exports = {
   googleClientID:
     "734685649440-kmtk1pfabimr6cn26lgnsh6hfr0iidl2.apps.googleusercontent.com",
   googleClientSecret: "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",
-  mongoURI: "mongodb+srv://127.0.0.1:27017/blog_ci",
+  mongoURI: "mongodb+srv://127.0.0.1/blog_ci",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6379",
   accessKeyId: "AKIAIWSXFHRM7F6Z3NWQ",
